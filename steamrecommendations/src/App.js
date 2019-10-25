7b9ab446a656b453e355fa2e568de835df7287bc
yangshu <yangshu377@yahoo.com>
2019-10-25 14:00:22 -0400

    set up router

--- steamrecommendations/src/App.js
+++ steamrecommendations/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Header from './common/header/index.js';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <div>
       <Header />
-    
+      <BrowserRouter>
+        <div>
+          <Route path = '/' exact render={()=><div>home</div>}></Route>
+          <Route path = '/detail' exact render={()=><div>detail</div>}></Route>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
